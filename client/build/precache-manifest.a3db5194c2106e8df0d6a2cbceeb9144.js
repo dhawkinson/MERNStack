4864a009e1fed0cdd46bcc8fa6d3acc708d95eb2
Doug Hawkinson <doug@webandappfactory.com>
2020-03-25 14:02:53 -0700

    Removed heroku postbuild from package.json. Ran manual build in client.

---
+++ client/build/precache-manifest.a3db5194c2106e8df0d6a2cbceeb9144.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c8fbb83a2095fb38f352ab6b7fedbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f1951beb20b0d466f83",
+    "url": "/static/css/main.78e18daa.chunk.css"
+  },
+  {
+    "revision": "9831e1022a7fc355f4ab",
+    "url": "/static/js/2.637295b5.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/static/js/2.637295b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f1951beb20b0d466f83",
+    "url": "/static/js/main.2a79a891.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
